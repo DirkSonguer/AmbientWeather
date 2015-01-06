bbc0c8a19fee3c3cc9a2be9641bcd16eb67f17a9
DirkSonguer <dirk@songuer.de>
2015-01-06 13:30:01 +0100

    Update as preparation to new version

--- translations/AmbientWeather.ts
+++ translations/AmbientWeather.ts
@@ -1,4 +1,29 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0">
+<context>
+    <name>About</name>
+    <message>
+        <source>AmbientWeather</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewLocation</name>
+    <message>
+        <source>Could not find any location with this name, sorry</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Temperature Scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
